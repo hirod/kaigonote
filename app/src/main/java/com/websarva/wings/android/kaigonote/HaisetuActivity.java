4855aa966c736013678dcdc2442b10dd2384c89e
miket <mikehiro6@gmail.com>
2021-10-03 20:23:09 +0900

    リファクタリング名前変更

--- app/src/main/java/com/websarva/wings/android/kaigonote/HaisetuActivity.java
+++ app/src/main/java/com/websarva/wings/android/kaigonote/HaisetuActivity.java
@@ -1,9 +1,7 @@
 package com.websarva.wings.android.kaigonote;
 
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,35 +13,23 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity2_1 extends AppCompatActivity {
-
+public class HaisetuActivity extends AppCompatActivity {
     private int _kaigoId = -1;
     private String _kaigoName = "";
-    private DatabaseHelper _helper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main21);
+        setContentView(R.layout.activity_haisetu);
         ListView kaigo = findViewById(R.id.kaigo);
         kaigo.setOnItemClickListener(new ListItemClickListner());
-        _helper = new DatabaseHelper(MainActivity2_1.this);
     }
 
-    @Override
-    protected void onDestroy(){
-    //DBヘルパーオブジェクトの解放。
-    _helper.close();
-    super.onDestroy();
-    }
-         // stmo = findViewById(R.id.stmo);
-        //stmo2 = findViewById(R.id.stmo2);
-       // sqLiteDatabase = databaseHelper.getWritableDatabase();
     public void onSaveButtonClick(View view){
         //名前を習得。
         EditText stmo = findViewById(R.id.stmo);
-        String note = stmo.getText().toString();
         stmo.setText("");
-
+        //備考を習得。
         EditText stmo2 = findViewById(R.id.stmo2);
         stmo2.setText("");
 
@@ -58,25 +44,11 @@ public class MainActivity2_1 extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?>parent,View view,int position,long id){
             _kaigoId = position;
-            _kaigoName = (String) parent.getItemAtPosition(position);
+            _kaigoName = (String)parent.getItemAtPosition(position);
             TextView tv_name = findViewById(R.id.tv_name);
             tv_name.setText(_kaigoName);
             Button tv_name1 = findViewById(R.id.tv_name1);
             tv_name1.setEnabled(true);
-
-            SQLiteDatabase db = _helper.getWritableDatabase();
-            String sql = "INSERT　INTO  Kaigomemo　VALUES　('排泄')" ;
-            String note;
-            try (Cursor cursor = db.rawQuery(sql, null)) {
-                note = "";
-
-                while (cursor.moveToNext()) {
-                    int idxNote = cursor.getColumnIndex("note");
-                    note = cursor.getString(idxNote);
-                }
-            }
-            EditText stmo = findViewById(R.id.stmo);
-            stmo.setText(note);
 
         }
     }
