1a3b00b06135e4e8ad1036ce3fbee88b5a016a8f
miket <mikehiro6@gmail.com>
2021-10-11 21:00:56 +0900

    binding,btclick名前変更前

--- app/src/main/java/com/websarva/wings/android/kaigonote/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/kaigonote/MainActivity.java
@@ -7,21 +7,28 @@ import android.view.View.OnClickListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.websarva.wings.android.kaigonote.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.bt_click).setOnClickListener(this);//排泄ボタンがタップされたときの処理
-        findViewById(R.id.bt_click2).setOnClickListener(this);//食事ボタンがタップされたときの処理
-        findViewById(R.id.bt_click3).setOnClickListener(this);//口腔ボタンがタップされたときの処理
-        findViewById(R.id.bt_click4).setOnClickListener(this);//入浴ボタンがタップされたときの処理
-        findViewById(R.id.bt_click5).setOnClickListener(this);//記録ボタンがタップされたときの処理
-        findViewById(R.id.bt_click6).setOnClickListener(this);//アクシデントボタンがタップされたときの処理
-        findViewById(R.id.bt_click7).setOnClickListener(this);//処置ボタンがタップされたときの処理
-        findViewById(R.id.bt_click8).setOnClickListener(this);//配薬ボタンがタップされたときの処理
-        findViewById(R.id.bt_click9).setOnClickListener(this);//記録一覧ボタンがタップされたときの処理
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        binding.btClick.setOnClickListener(this);//排泄ボタンがタップされたときの処理
+        binding.btClick2.setOnClickListener(this);//食事ボタンがタップされたときの処理
+        binding.btClick3.setOnClickListener(this);//口腔ボタンがタップされたときの処理
+        binding.btClick4.setOnClickListener(this);//入浴ボタンがタップされたときの処理
+        binding.btClick5.setOnClickListener(this);//記録ボタンがタップされたときの処理
+        binding.btClick6.setOnClickListener(this);//アクシデントボタンがタップされたときの処理
+        binding.btClick7.setOnClickListener(this);//処置ボタンがタップされたときの処理
+        binding.btClick8.setOnClickListener(this);//配薬ボタンがタップされたときの処理
+        binding.btClick9.setOnClickListener(this);//記録一覧ボタンがタップされたときの処理
 
     }
 
