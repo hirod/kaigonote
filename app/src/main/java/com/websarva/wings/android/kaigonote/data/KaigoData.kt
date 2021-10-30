bf475a92fa9323978b8cd74ac026d9e4d06c42f6
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-10-31 07:37:55 +0900

    UIDを追加

--- app/src/main/java/com/websarva/wings/android/kaigonote/data/KaigoData.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/data/KaigoData.kt
@@ -7,52 +7,52 @@ import java.util.*
 
 @Entity(tableName = "resident")
 data class Resident(
-        @PrimaryKey(autoGenerate = true)
-        val uid: Long,
-        @ColumnInfo(name = "name")
-        val name: String?,
-        @ColumnInfo(name = "date")
-        val birthday: Date
+    @PrimaryKey(autoGenerate = true)
+    val uid: Long,
+    @ColumnInfo(name = "name")
+    val name: String?,
+    @ColumnInfo(name = "date")
+    val birthday: Date
 )
 
 @Entity(tableName = "Kaigo")//追加
 data class Kaigo(
-        @PrimaryKey(autoGenerate = true)
-        val uid: Long,
-        var hiduke: Date,
-        var roomNumber: Int,
-        var riyousyaID: Int,
-        var hinyou: String,
-        var naibenn: String,
-        var nyuyoku: String,
-        var tyousyokuSyusyoku: Int,
-        var tyousyokuFukusyoku: Int,
-        var tyousyokuInsui: Int,
-        var tyusyokuSyusyoku: Int,
-        var tyusyokuFukusyoku: Int,
-        var tyusyokuInsui: Int,
-        var yusyokuSyusyoku: Int,
-        var yusyokuFukusyoku: Int,
-        var yusyokuInsui: Int,
-        var kansyoku: Int,
-        var kansyokuInsui: Int,
-        var tyousyokuGoyaku: String,
-        var tyusyokuGoyaku: String,
-        var yusyokuGoyaku: String,
-        var kisyouziyaku: String,
-        var syusinnyaku: String,
-        var tonpuku: String,
-        var tyousyokuZenyaku: String,
-        var tyusyokuZenyaku: String,
-        var yusyokuZenyaku: String,
-        var koukuAsa: String,
-        var koukuHiru: String,
-        var koukuYoru: String,
-        var nitizyoukiroku: String,
-        var nyuyokukiroku: String,
-        var mousiokuri: String,
-        var hiyari: String,
-        var zikohoukokusyo: String,
-        var kirokuitirann: String,
+    @PrimaryKey(autoGenerate = true)
+    val uid: Long,
+    var hiduke: Date,
+    var roomNumber: Int,
+    var riyousyaID: Int,
+    var hinyou: String,
+    var naibenn: String,
+    var nyuyoku: String,
+    var tyousyokuSyusyoku: Int,
+    var tyousyokuFukusyoku: Int,
+    var tyousyokuInsui: Int,
+    var tyusyokuSyusyoku: Int,
+    var tyusyokuFukusyoku: Int,
+    var tyusyokuInsui: Int,
+    var yusyokuSyusyoku: Int,
+    var yusyokuFukusyoku: Int,
+    var yusyokuInsui: Int,
+    var kansyoku: Int,
+    var kansyokuInsui: Int,
+    var tyousyokuGoyaku: String,
+    var tyusyokuGoyaku: String,
+    var yusyokuGoyaku: String,
+    var kisyouziyaku: String,
+    var syusinnyaku: String,
+    var tonpuku: String,
+    var tyousyokuZenyaku: String,
+    var tyusyokuZenyaku: String,
+    var yusyokuZenyaku: String,
+    var koukuAsa: String,
+    var koukuHiru: String,
+    var koukuYoru: String,
+    var nitizyoukiroku: String,
+    var nyuyokukiroku: String,
+    var mousiokuri: String,
+    var hiyari: String,
+    var zikohoukokusyo: String,
+    var kirokuitirann: String,
 
-        )
+    )
