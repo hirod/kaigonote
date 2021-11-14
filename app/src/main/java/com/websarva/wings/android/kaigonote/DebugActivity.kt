8a1ec6491c099c2b8e36fcb63e18b941cfebb0ae
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-11-14 19:53:55 +0900

    デバッグ画面のレイアウトを追加

--- app/src/main/java/com/websarva/wings/android/kaigonote/DebugActivity.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/DebugActivity.kt
@@ -1,11 +1,30 @@
 package com.websarva.wings.android.kaigonote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.websarva.wings.android.kaigonote.databinding.ActivityDebugBinding
 
 class DebugActivity : AppCompatActivity() {
+
+    private var binding: ActivityDebugBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_debug)
+        binding = ActivityDebugBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.clearHainyou.setOnClickListener {
+            clearHainyou()
+        }
+
+        binding!!.addHainyou.setOnClickListener {
+            addHainyou()
+        }
+    }
+
+    private fun clearHainyou() {
+    }
+
+    private fun addHainyou() {
     }
 }
