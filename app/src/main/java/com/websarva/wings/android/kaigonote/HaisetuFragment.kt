5e917c20d4b788a594b50556640c37cbe93e353e
miket <mikehiro6@gmail.com>
2021-11-21 21:15:06 +0900

    HaisetuFragment.kt編集

--- app/src/main/java/com/websarva/wings/android/kaigonote/HaisetuFragment.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/HaisetuFragment.kt
@@ -1,59 +1,78 @@
 package com.websarva.wings.android.kaigonote
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.websarva.wings.android.kaigonote.data.Hainyou
+import com.websarva.wings.android.kaigonote.data.KaigoDB
+import com.websarva.wings.android.kaigonote.databinding.FragmentHaisetuBinding
+import com.websarva.wings.android.kaigonote.databinding.ItemLogBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HaisetuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HaisetuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentHaisetuBinding
+    private lateinit var adapter: ArrayAdapter<Hainyou>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_haisetu, container, false)
+    ): View {
+        binding = FragmentHaisetuBinding.inflate(inflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HaisetuFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HaisetuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = object : ArrayAdapter<Hainyou>(requireContext(), R.layout.item_log) {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                val data = getItem(position)!!
+                var cv = convertView
+                if (cv == null) {
+                    cv = layoutInflater.inflate(R.layout.item_log, parent, false)
+
                 }
+                val itemBinding = ItemLogBinding.bind(cv!!)//画面表示する
+                itemBinding.date.text = data.hiduke.toString()
+                itemBinding.name.text = data.name
+                itemBinding.haisetu.text = data.hainyou
+                return cv
             }
+        }
+        binding.list.adapter = adapter
+        loadLog(0)
+    }
+
+    private fun loadLog(offset: Long) {
+        lifecycleScope.launch {
+            // データベースから読み込み
+            val list = withContext(Dispatchers.IO) {
+                val db = KaigoDB.getInstance(requireContext().applicationContext as Application)
+                val dao = db.hainyou()
+                dao.gethainyouAll(offset)
+            }
+            adapter.clear()
+            adapter.addAll(list)
+            //画面に表示
+        }
+        
     }
 }
+
+
+
--- app/src/main/java/com/websarva/wings/android/kaigonote/KirokuitirannActivity.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/KirokuitirannActivity.kt
@@ -2,25 +2,14 @@ package com.websarva.wings.android.kaigonote
 
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.google.android.material.tabs.TabLayoutMediator
-import com.websarva.wings.android.kaigonote.data.Hainyou
-import com.websarva.wings.android.kaigonote.data.KaigoDB
-import com.websarva.wings.android.kaigonote.databinding.ItemLogBinding
 import com.websarva.wings.android.kaigonote.databinding.KirokuitirannBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class KirokuitirannActivity : AppCompatActivity() {
 
-
     private lateinit var viewPagerAdapter: ViewPagerAdapter
     private lateinit var binding: KirokuitirannBinding
-    private lateinit var adapter: ArrayAdapter<Hainyou>
     private var page: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,51 +38,12 @@ class KirokuitirannActivity : AppCompatActivity() {
                 }
             }.attach()
         }
-
-        adapter = object : ArrayAdapter<Hainyou>(this, R.layout.item_log) {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-                val data = getItem(position)!!
-                var cv = convertView
-                if (cv == null) {
-                    cv = layoutInflater.inflate(R.layout.item_log, parent, false)
-
-                }
-                val itemBinding = ItemLogBinding.bind(cv!!)//画面表示する
-                itemBinding.date.text = data.hiduke.toString()
-                itemBinding.name.text = data.name
-                itemBinding.haisetu.text = data.hainyou
-                return cv
-            }
-
-        }
-        binding.list.adapter = adapter
-        binding.next.setOnClickListener {
-            page++
-            loadLog((page * 100).toLong())
-        }
-        binding.back.setOnClickListener {
-            if (page > 0) {
-                page--
-            }
-            loadLog((page * 100).toLong())
-        }
-
-        //読み込み
-        loadLog(0)
     }
-
-    private fun loadLog(offset: Long) {
-        lifecycleScope.launch {
-            // データベースから読み込み
-            val list = withContext(Dispatchers.IO) {
-                val db = KaigoDB.getInstance(application)
-                val dao = db.hainyou()
-                dao.gethainyouAll(offset)
-            }
-            adapter.clear()
-            adapter.addAll(list)
-            //画面に表示
-        }
-    }
-
 }
+
+
+/*
+
+  //読み込み
+  loadLog(0))*/
+
