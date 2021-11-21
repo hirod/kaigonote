3210486633ddfb2d462ef303926faf0fea158314
Toshihiro Sekioka <toshihiro_sekioka@yayoi-kk.co.jp>
2021-11-21 10:39:47 +0900

    排便全削除

--- app/src/main/java/com/websarva/wings/android/kaigonote/data/HainbenDao.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/data/HainbenDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import java.util.*
+import java.util.Date
 
 @Dao
 interface HaibenDao {
@@ -19,4 +19,7 @@ interface HaibenDao {
 
     @Delete
     fun delete(haiben: Haiben)
+
+    @Query("DELETE FROM haiben")
+    fun deleteAll()
 }
