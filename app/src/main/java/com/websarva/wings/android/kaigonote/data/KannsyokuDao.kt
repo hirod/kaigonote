d7910add1458c3e07488e3f10f33942473dd3ec9
miket <mikehiro6@gmail.com>
2021-12-08 22:44:03 +0900

    kansyoku.xml 作成

--- app/src/main/java/com/websarva/wings/android/kaigonote/data/KannsyokuDao.kt
+++ app/src/main/java/com/websarva/wings/android/kaigonote/data/KannsyokuDao.kt
@@ -4,15 +4,22 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import java.util.*
 
 @Dao
 interface KannsyokuDao {
-    @Query("SELECT * FROM kannsyoku")
-    fun getkannsyokuAll(): List<Kannsyoku>
+    @Query("SELECT * FROM kannsyoku LIMIT 100 OFFSET :offset")
+    fun getkannsyokuAll(offset: Long): List<Kannsyoku>
+
+    @Query("SELECT * FROM kannsyoku WHERE hiduke =:hiduke LIMIT 100 OFFSET :offset")
+    fun getkannsyokuByhiduke(hiduke: Date, offset: Long): List<Kannsyoku>
 
     @Insert
     fun insert(kannsyoku: Kannsyoku)
 
     @Delete
     fun delete(kannsyoku: Kannsyoku)
+
+    @Query("DELETE FROM kannsyoku")
+    fun deleteAll()
 }
